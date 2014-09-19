825b8cb881ba5a28be3f362c800eebda31e14911
Samantha McCormick <omgitzme@gmail.com>
2014-09-19 11:18:47 -0600

    Finished part 2

--- part-2-editable.js
+++ part-2-editable.js
@@ -1,18 +1,28 @@
 $(document).on('ready', function() {
 
 	$(document).on("click", "p", function() {
-		$("p").addClass("editable");
-		$(this).after("<textarea></textarea>");
-		$("textarea").defaultValue = $(this);
+		$(".edit").addClass("editable");
+		$(this).hide();
+		$(this).replaceWith("<p class = 'edit'><textarea></textarea></p>");
+		$("textarea").val( $(this).text() );   // Put the thing you clicked's text into the textarea val()
 		$("textarea").focus();  // Auto-focus the textarea
+	});
 
+	$(document).on("click", "h1", function() {
+		$(".edit").addClass("editable");
+		$(this).hide();
+		$(this).replaceWith("<h1 class = 'editHeader'><textarea></textarea></h1>");
+		$("textarea").val( $(this).text() );   // Put the thing you clicked's text into the textarea val()
+		$("textarea").focus();  // Auto-focus the textarea
 	});
 
 	$(document).on("blur", "textarea", function() {
-		var enteredText = $("textarea").val();
-		$("textarea").hide();
-		$("img").after(enteredText);
+		var enteredText = $("textarea").val();  // Entered text is the NEW textarea's val()
+		$("textarea").hide();   // Hide the textarea
+		$("textarea").replaceWith(enteredText);  // Put the entered text after the textarea
 	});
+
+
 
 
 
